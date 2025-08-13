9f2a459dc7e58ff1f441dcd4699e480491e7fa3c
alexfisher03 <fisherr.alexander@gmail.com>
2025-08-13 15:12:17 -0400

    Update

--- QuietStore/api/v1/routes.go
+++ QuietStore/api/v1/routes.go
@@ -2,17 +2,10 @@ package v1
 
 import (
 	"github.com/alexfisher03/quietstore-service/QuietStore/internal/handlers"
-	"github.com/alexfisher03/quietstore-service/QuietStore/internal/service"
 	"github.com/gofiber/fiber/v2"
 )
 
-func RegisterRoutes(app *fiber.App, storageService service.StorageService) {
-
-	fileHandler := handlers.NewFileHandler(storageService)
-
+func RegisterRoutes(app *fiber.App) {
 	v1 := app.Group("/api/v1")
 	v1.Get("/health", handlers.HealthCheck)
-	v1.Post("/files/upload", fileHandler.UploadFileHandler)
-	v1.Get("/files/:id", fileHandler.GetFileByIdHandler)
-	v1.Get("/files", fileHandler.GetAllFilesForUserHandler)
 }
--- QuietStore/internal/handlers/health.go
+++ QuietStore/internal/handlers/health.go
@@ -8,7 +8,7 @@ import (
 
 func HealthCheck(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
-		"status":    "OK",
+		"status":    "ok",
 		"service":   "QuietStore",
 		"timestamp": time.Now().Format(time.RFC3339),
 	})
